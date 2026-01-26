25b07390a4d3d4c86d6f05a3ba30e2b3e3c91d31
EddieCherian <111652097+EddieCherian@users.noreply.github.com>
2026-01-25 22:46:27 -0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SmileStreak from "./SmileStreak";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SmileStreak />
+  </React.StrictMode>
+);

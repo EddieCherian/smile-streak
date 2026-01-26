39cd6e770bc37b93b9d9bd7cf1baaf220cef59cf
EddieCherian <111652097+EddieCherian@users.noreply.github.com>
2026-01-26 10:51:35 -0600

    Update Today.jsx

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -1,3 +1,7 @@
+const BRUSH_TIME = 120; // 2 minutes in seconds
+
+const [timer, setTimer] = useState(null); // "morning" | "night" | null
+const [timeLeft, setTimeLeft] = useState(BRUSH_TIME);
 habitData
 setHabitData
 todayKey

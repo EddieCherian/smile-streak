0a773553e7fe3b58a95815acd6ab4e3be8cc5b48
EddieCherian <111652097+EddieCherian@users.noreply.github.com>
2026-01-25 23:36:00 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SmileStreak from "./SmileStreak";
+import App from "./App";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SmileStreak />
+    <App />
   </React.StrictMode>
 );

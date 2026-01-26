f4239f1452548d173ab25e4b42d3d7cda172e764
EddieCherian <111652097+EddieCherian@users.noreply.github.com>
2026-01-26 09:28:57 -0600

    Create date.js

---
+++ src/utils/date.js
@@ -0,0 +1,13 @@
+export const getLocalMidnight = (date = new Date()) => {
+  const d = new Date(date);
+  d.setHours(0, 0, 0, 0);
+  return d;
+};
+
+export const getDateKey = (date = new Date()) => {
+  const d = getLocalMidnight(date);
+  const y = d.getFullYear();
+  const m = String(d.getMonth() + 1).padStart(2, "0");
+  const day = String(d.getDate()).padStart(2, "0");
+  return `${y}-${m}-${day}`;
+};
